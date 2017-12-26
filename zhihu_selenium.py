d2ca9dcc4a827c93e141944f52efb562e8f07ffa
Zizhuo Zhang <zzz18287108118@163.com>
2017-12-26 19:04:16 +0800

    Add files via upload

---
+++ zhihu_selenium.py
@@ -0,0 +1,232 @@
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
+import sys
+import re
+
+class zhihuSpider(object):
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.homePageUrl = 'https://www.zhihu.com/'
+        self.topic = '游戏'
+        self.topicUrl = 'https://www.zhihu.com/topic#' + self.topic
+        self.account = '18287108118'
+        self.password = 'zzz970504'
+        self.times = 20
+        self.QueTitle = []
+        self.QueFirstAns = []
+        self.QueUrl = []
+        self.QueDic = {
+            'url': self.QueUrl,
+            'title': self.QueTitle,
+            'author': self.QueFirstAns
+        }
+        self.Ans = []
+
+    def login(self):
+        try:
+            self.driver.get(self.homePageUrl)
+            self.driver.find_element_by_name('username').send_keys(self.account)
+            time.sleep(1)
+            self.driver.find_element_by_name('password').send_keys(self.password)
+            time.sleep(1)
+            self.driver.find_element_by_class_name('SignFlow-submitButton').click()
+            time.sleep(1)
+        except:
+            print('login error!:(')
+            sys.exit(-1)
+
+    def _getTopicHtml(self, url=None):
+        try:
+            self.driver.get(url)
+            self._windowScroll(self.times)
+            return self.driver.page_source
+        except:
+            print('get question Html error!:(')
+            return ""
+
+    def _getQuestionInfo(self, html=None):
+        try:
+            soup = BeautifulSoup(html, 'html.parser')
+            queTag = soup.find_all('a', attrs={'class': 'question_link'})
+            queFirstAnsTag = soup.find_all('a', attrs={'class': 'author-link'})
+            for i in range(len(queTag)):
+                try:
+                    self.QueUrl.append(re.sub('\n', '', 'https://www.zhihu.com' + queTag[i]['href']))
+                    self.QueTitle.append(re.sub('\n', '', queTag[i].string))
+                    self.QueFirstAns.append(re.sub('\n', '', queFirstAnsTag[i].string))
+                except:
+                    continue
+        except:
+            print('get question url error!:(')
+            sys.exit(-1)
+
+    def _windowScroll(self, times):
+        for i in range(times + 1):
+            self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+            time.sleep(3)
+
+    def _saveQuestion(self):
+        k = 1
+        with open('question.txt', 'w', encoding='utf-8') as f:
+            for i in range(len(self.QueUrl)):
+                try:
+                    f.write(str(k) + '\t' + '标题:' + str(self.QueTitle[i]) + '\n' +
+                            '\t' + '回答者:' + str(self.QueFirstAns[i]) + '\n' +
+                            '\t' + 'url:' + str(self.QueUrl[i]))
+                    f.write('\n')
+                    k = k + 1
+                except:
+                    continue
+
+    def getQuestion(self):
+        html = self._getTopicHtml(self.topicUrl)
+        self._getQuestionInfo(html)
+        self._saveQuestion()
+
+    def _getAnswerHtml(self, url=None):
+        try:
+            self.driver.get(url)
+            try:
+                self.driver.find_element_by_class_name('QuestionRichText-more').click()
+                time.sleep(1)
+            except:
+                pass
+            self._windowScroll(self.times)
+            return self.driver.page_source
+        except:
+            print('get answer html failed!:(')
+            return ""
+
+    def _getAnswerInfo(self, html=None):
+        ansInfo = AnsPage(html)
+        ansInfo.getAnsInfoDic()
+        ansDic = ansInfo.ansInfoDic
+        return ansDic
+
+    def _saveAns(self, infoDic=None, i=0):
+        fpath = './answer/answer' + str(i+1) + '.txt'
+        with open(fpath, 'w', encoding='utf-8') as f:
+            f.write('问题 ' + str(i+1) + '\n')
+            f.write('问题标题:' + '\t' + str(self.QueTitle[i]) + '\n')
+            f.write('问题描述:' + '\t' + str(infoDic['queDis']) + '\n\n')
+            for j in range(len(infoDic['ansAuthor'])):
+                try:
+                    f.write('回答者' + str(j+1) + ':' + '\t' + str(infoDic['ansAuthor'][j]) + '\n')
+                    f.write('点赞数:' + '\t' + str(infoDic['ansPraise'][j]) + '\n')
+                    f.write('回答内容:' + '\t' + str(infoDic['ansText'][j]) + '\n\n')
+                except:
+                    continue
+
+    def getAnswer(self):
+        index = 0
+        for url in self.QueUrl:
+            html = self._getAnswerHtml(url)
+            ansInfoDic = self._getAnswerInfo(html)
+            self.Ans.append(ansInfoDic)
+            self._saveAns(ansInfoDic, index)
+            index = index + 1
+
+    def runSpider(self):
+        self.login()
+        self.getQuestion()
+        self.getAnswer()
+
+
+class AnsPage(object):
+    def __init__(self, html):
+        self.html = html
+        self.soup = BeautifulSoup(self.html, 'html.parser')
+        self.queDis = ''
+        self.ansAuthor = []
+        self.ansPraise = []
+        self.ansText = []
+        self.ansInfoDic = {
+            'queDis': self.queDis,
+            'ansAuthor': self.ansAuthor,
+            'ansPraise': self.ansPraise,
+            'ansText': self.ansText
+        }
+
+    def _getQueDiscribe(self):
+        try:
+            queDisTag = self.soup.find('span', attrs={'class': 'RichText', 'itemprop': 'text'})
+            if queDisTag.get('class') == ['RichText']:
+                queDis = queDisTag.string
+            else:
+                queDis = None
+            return queDis
+        except:
+            print('get question discription failed!')
+            return None
+
+    def _getAnswerer(self):
+        try:
+            authorPic = self.soup.find_all('img', attrs={'class': ['Avatar', 'AuthorInfo-avatar'],
+                                                         'width': '38', 'height': '38'})
+            author = []
+            for elem in authorPic:
+                author.append(elem.get('alt'))
+            return author
+        except:
+            print('get answerer failed!:(')
+            return None
+
+    def _getAnsPraise(self):
+        try:
+            praiseTag = self.soup.find_all('button', attrs={
+                'class': ['Button', 'VoteButton', 'VoteButton--up'], 'aria-label': '赞同'})
+            praiseNum = []
+            for elem in praiseTag:
+                try:
+                    for string in elem.strings:
+                        praiseNum.append(string)
+                except:
+                    continue
+            return praiseNum
+        except:
+            print('get praise number failed!:(')
+            return None
+
+    def _getAnsText(self):
+        try:
+            ansTextTag = self.soup.find_all('span', attrs={'class': ['RichText', 'CopyrightRichText-richText'],
+                                                      'itemprop': 'text'})
+            ansTextTag = [elem for elem in ansTextTag
+                          if elem.get('class') == ['RichText', 'CopyrightRichText-richText']]
+            ansText = []
+            tempText = ''
+            for i in range(len(ansTextTag)):
+                try:
+                    try:
+                        while True:
+                            ansTextTag[i].figure.decompose()
+                    except:
+                        for child_str in ansTextTag[i].strings:
+                            tempText = tempText + child_str
+                        ansText.append(tempText)
+                        tempText = ''
+                except:
+                    continue
+            return ansText
+        except:
+            print('get answer text failed!:(')
+            return None
+
+    def getAnsInfoDic(self):
+        self.queDis = self._getQueDiscribe()
+        self.ansAuthor = self._getAnswerer()
+        self.ansPraise = self._getAnsPraise()
+        self.ansText = self._getAnsText()
+        self.ansInfoDic = {
+            'queDis': self.queDis,
+            'ansAuthor': self.ansAuthor,
+            'ansPraise': self.ansPraise,
+            'ansText': self.ansText
+        }
+
+
+if __name__ == '__main__':
+    zhihu = zhihuSpider()
+    zhihu.runSpider()
+    print(len(zhihu.QueUrl))
